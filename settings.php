881929bd3ae297589d4280c7db78bd433f82b260
Jay <jay.s@eyebuydirect.com>
2017-08-15 17:06:13 +0800

    init

--- settings.php
+++ settings.php
@@ -1,37 +1,44 @@
 <?php
 
 //template language,english, french or zh
-$blog_config['language']	= 'english';
+$blog_config['language'] = 'zh';
 
 //blog title
-$blog_config['title'] = 'Just Writing';
+$blog_config['title'] = '猪哥亮的小九九';
+
 //blog sub title
-$blog_config['intro'] = 'Super Simple Blog Engine';
+$blog_config['intro'] = ' 总得说点什么。。。 ';
+
 //blog author
-$blog_config['author']='JustWriting';
+$blog_config['author'] = '猪哥亮';
+
 //"About Me" box
-$blog_config['aboutme']='This is about JustWriting';
+$blog_config['aboutme'] = ' 在下姓猪哥，单名一个亮字，号半猿。 web全栈程序猿 · lnmp + react + node · and so on. ';
+
 //blog avatar
-$blog_config['avatar']= 'https://raw.githubusercontent.com/hjue/JustWriting/develop/docs/logo.png';
+$blog_config['avatar'] = 'http://oj6n9nf7i.bkt.clouddn.com/image/test/head.jpg';
 
 //blog template name.The template root path is /templates.You can set rock or deepure. 
 $blog_config['template'] = 'rock';
 
 //If you would like that everyone comment your post,you must set this variable to True.
 $blog_config['comment'] = True;
+
 //duoshuo short name，duoshuo is the social comment system,url is  http://duoshuo.com/.
 $blog_config['duoshuo_short_name'] = 'justwriting';
+
 //disqus short name
 $blog_config['disqus_short_name'] = '';
 
 //URL to your blog root.This is your base URL,without  a trailing slash:http://justwriting.sinaapp.com
-$blog_config['base_url'] = '';
+$blog_config['base_url'] = 'http://jw.fsociety.cn/';
 
 //your github url
-$blog_config['github'] = 'https://github.com/hjue/JustWriting';
+$blog_config['github'] = 'https://github.com/zhugeliange';
 
 //other social network : twitter, facebook, rss and email. For example :
-$blog_config['twitter'] = '';
+$blog_config['facebook'] = 'https://www.facebook.com/its.funnuy';
+$blog_config['weibo'] = 'http://weibo.com/1803651761/profile?topnav=1&wvr=6';
 //there's more supported networks, but you must add them manually to \templates\rock\base.html - names are in \templates\rock\images\social\*
 
 //posts count of posts list
@@ -39,6 +46,7 @@ $blog_config['posts_per_page'] = 10;
 
 //on or off API.  API doc : https://github.com/hjue/JustWriting/wiki/API .
 $blog_config['api'] = False;
+
 //api key
 $blog_config['api_key'] = '1234561';
 
@@ -52,15 +60,3 @@ $blog_config['dropbox']['access_token']= '';
  * Support 49 code styles, pls see this page: https://highlightjs.org/static/test.html
  */
 $blog_config['highlight']='default';
-
-//remove comment to support Latex math equations
-/*
-$blog_config['mathjax']='<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=default">
-MathJax.Hub.Config({
-    tex2jax: {
-         inlineMath: [ ["$","$"]]
-         },
-     extensions: ["jsMath2jax.js", "tex2jax.js"],
-     messageStyle: "none"
- });</script>';
-*/
