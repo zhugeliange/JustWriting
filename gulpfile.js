304eaf78d1d32aacf5a1173412f1cba63a595d15
Jay <jay.s@eyebuydirect.com>
2017-09-01 15:19:07 +0800

    finish

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var gulp = require('gulp'), // gulp基础库
 gulp.task('minifycss', function() {
     return gulp.src('templates/rock/static/src/css/**/*.css') // 设置css
         .pipe(minifycss()) // 压缩文件
-        .pipe(changed('templates/rock/static/dist/css/')),
+        .pipe(changed('templates/rock/static/dist/css/'))
         .pipe(gulp.dest('templates/rock/static/dist/css/')) // 设置输出路径
         .pipe(notify({
             message: 'css task ok'
@@ -24,7 +24,7 @@ gulp.task('minifyjs', function() {
     return gulp.src('templates/rock/static/src/js/**/*.js')
         .pipe(jshint()) // 检查js
         .pipe(uglify()) // 压缩文件
-        .pipe(changed('templates/rock/static/dist/js/')),
+        .pipe(changed('templates/rock/static/dist/js/'))
         .pipe(gulp.dest('templates/rock/static/dist/js/')) // 设置输出路径
         .pipe(notify({
             message: "js task ok"
@@ -37,10 +37,10 @@ gulp.task('minifyimage', function() {
         .pipe(imagemin({
             progressive: true
         })) // 压缩文件
-        .pipe(changed('templates/rock/static/dist/images/')),
+        .pipe(changed('templates/rock/static/dist/images/'))
         .pipe(gulp.dest('templates/rock/static/dist/images/')) // 设置输出路径
         .pipe(notify({
-            message: "iamge task ok"
+            message: "image task ok"
         })); // 提示成功
 });
 
